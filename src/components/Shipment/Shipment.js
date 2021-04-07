e3160cb830ca078dc62333a0eb85947234a4d319
Umme Salma Ali <ummes3527@gmail.com>
2021-04-07 21:28:50 +0600

    more-features added

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,7 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import UserContext from '../../UserContext';
 import './Shipment.css'
+
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 const Shipment = () => {
     const [user,setUser] = useContext(UserContext)
@@ -11,10 +20,39 @@ const Shipment = () => {
     };
   
     console.log(watch("example")); 
+    const [selectedDate, setSelectedDate] = useState({
+      OrderDate: new Date(),
+      DeliveryDate: new Date(),
+    });
+
+  const handleOrder = (date) => {
+    const newDates = {...selectedDate}
+    newDates.OrderDate = date;
+    setSelectedDate(newDates);
+  };
+  const handleDelivery = (date) => {
+    const newDates = {...selectedDate}
+    newDates.DeliveryDate = date;
+    setSelectedDate(newDates);
+  };
+   
+  const handleBooking = () =>{
+   const newOrder = {...selectedDate, ...user}
+   fetch('http://localhost:5000/addOrder',{
+     method: 'POST',
+     headers: {'Content-type': 'application/json'},
+     body:JSON.stringify(newOrder)
+   })
+   .then(res => res.json())
+   .then(data =>{
+     console.log(data);
+   })
+  }
   
     return (
         
-      <form className='ship-form' onSubmit={handleSubmit(onSubmit)}>
+     <div>
+        <form className='ship-form' onSubmit={handleSubmit(onSubmit)}>
         
         
         <input name='name' defaultValue={user.name}  {...register("exampleRequired", { required: true })} placeholder='Your Name' />
@@ -26,8 +64,41 @@ const Shipment = () => {
         <input name='phone'  {...register("exampleRequired", { required: true })} placeholder='Your Cell phone no' />
         {errors.name && <span className='error' >Phone is required</span>}
         <br/>
-        <button className="btn btn-success">Place Order</button>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="dd/MM/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Order Date"
+          value={selectedDate.OrderDate}
+          onChange={handleOrder}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Probable Delivery Date"
+          format="dd/MM/yyyy"
+          value={selectedDate.DeliveryDate}
+          onChange={handleDelivery}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        
+      </Grid>
+    </MuiPickersUtilsProvider>
+        <button onClick={handleBooking()} className="btn btn-primary 'text-center'">Place Order</button>
       </form>
+     
+
+     </div>
+      
     );
 };
 
